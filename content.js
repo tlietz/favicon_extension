ecb6d238e4232a59cc397a5ae810e4e48b7fd3fd
tlietz <theodore.lietz@gmail.com>
2021-03-02 08:29:17 -0600

    larger favicon grabbing & code refactor

--- content.js
+++ content.js
@@ -1,31 +1,39 @@
 const baseUrl = document.location.origin;
 let favicon = "";
 let domain = "";
+let mode = "default"; //mode can either be 'default' or 'large'
 
-// Google search has 'cite' html tag for easier access to domain name so its query selector
-// has a different querySelector parameter than other websites.
 if (baseUrl == "https://www.google.com") {
     const externalLinks = document.body.querySelectorAll("cite");
 
     // Insert favicons
-    for (link of externalLinks) {
-        // link.style.background = "green";
-        domain = link.innerText.split(" ")[0];
-        favicon = getFavicon(domain);
-        link.before(favicon);
+    if (mode === "default") {
+        for (link of externalLinks) {
+            domain = link.innerText.split(" ")[0];
+            favicon = getFavicon(domain, 16);
+            favicon.classList.add("favicon-16");
+            link.before(favicon);
+        }
+    } else {
+        for (link of externalLinks) {
+            domain = link.innerText.split(" ")[0];
+            favicon = getFavicon(domain, 32);
+            favicon.classList.add("favicon-32");
+            link.before(favicon);
+        }
     }
 }
 
-// Returns the favicon of domain as an image, returns default favicon provided by Google if none are found
-// where domain is of the form www.website.com or website.com
-function getFavicon(domain) {
+// Returns the favicon of domain as an image,
+// returns default globe favicon provided by Google if none are found.
+// Where domain is of the form www.website.com or website.com
+function getFavicon(domain, faviconSize) {
     const favicon_img = document.createElement("img");
     favicon_img.src =
-        "https://s2.googleusercontent.com/s2/favicons?domain=" + domain;
+        "https://s2.googleusercontent.com/s2/favicons?sz=" +
+        faviconSize +
+        "&domain=" +
+        domain;
     // "https://" + domain + "/favicon.ico";
-    favicon_img.classList.add("favicon");
     return favicon_img;
 }
-
-// Inserts favicons to all links
-// function insertFavicons(externalLinks) {}
