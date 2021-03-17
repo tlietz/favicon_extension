ad6b5eec1d540f2700758b13e957f09f9f723a05
tlietz <theodore.lietz@gmail.com>
2021-03-17 09:01:01 -0500

    started popup

--- content.js
+++ content.js
@@ -1,29 +1,21 @@
 let favicon = "";
 let domain = "";
 let link = "";
-let mode = ""; //mode can either be 'default' or 'large'
+let mode = "large"; //mode can either be 'small' or 'large'
 
 const externalLinks = document.body.querySelectorAll("div.TbwUpd cite.iUh30");
 // Insert favicons
-if (mode === "default") {
-    for (i = 0; i < externalLinks.length; i += 2) {
-        link = externalLinks[i];
-        domain = link.innerText.split(" ")[0];
-        favicon = getFavicon(domain, 16);
-        favicon.classList.add("favicon-16");
-        link.before(favicon);
-    }
-} else {
-    for (i = 0; i < externalLinks.length; i += 2) {
-        link = externalLinks[i];
-        domain = link.innerText.split(" ")[0];
-        linkPosition = link.getBoundingClientRect().top;
-        // Test
+for (i = 0; i < externalLinks.length; i += 2) {
+    link = externalLinks[i];
+    domain = link.innerText.split(" ")[0];
+    if (mode === "large") {
         favicon = getFavicon(domain, 32);
         favicon.classList.add("favicon-32");
-        // offset for getbounding client rect since it measures the entire page
-        link.before(favicon);
+    } else {
+        favicon = getFavicon(domain, 16);
+        favicon.classList.add("favicon-16");
     }
+    link.before(favicon);
 }
 
 // returns default globe favicon provided by Google if none are found.
